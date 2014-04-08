a92c83a83d210d6f33e692082481a7375813584d
Jesse <jesseluisdiaz@gmail.com>
2014-04-08 16:41:11 +0900

    update

--- resources/public/js/site.js
+++ resources/public/js/site.js
@@ -1,18 +1,10 @@
-/***
- * Excerpted from "Web Development with Clojure",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/dswdcloj for more book information.
-***/
 function colorStr(color) {
     return "rgb("+color[0]+","+color[1]+","+color[2]+")";
 }
 
-function setColor(div, colors) {    
+function setColor(div, colors) {
     var bgColor = colors[0];
-    var textColor = colors[1];    
+    var textColor = colors[1];
     div.css("background-color", colorStr(bgColor));
     div.find('a').css("color", colorStr(textColor));
 }
@@ -24,10 +16,10 @@ $(document).ready(function(){
              var div = $(this);
              var url = div.find('img').attr('src');
              var thumbColors = new AlbumColors(url);
-        
-            var color = "";         
+
+            var color = "";
             thumbColors.getColors(function(colors) {
             setColor(div, colors);
         });
-    });    
+    });
 });
